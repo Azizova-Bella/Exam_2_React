768bfc23e010ae840de8c33215b484aebd2dc7b4
Azizova-Bella <azizovabella14@gmail.com>
2025-02-20 05:40:10 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaTrash, FaEdit } from "react-icons/fa";
+import { FaBeer } from "react-icons/fa";
 import "./App.css";
 
 function App() {
@@ -204,7 +204,7 @@ function App() {
                     className="modalSave"
                     onClick={editTaskId ? editTask : addTask}
                   >
-                    Save
+                    {editTaskId ? "Save" : "Add"}
                   </button>
                   <button className="modalCancel" onClick={closeModal}>
                     Cancel
